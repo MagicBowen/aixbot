f88a3fccad12eac23a2d4cad28040ce6fbb05956
magicbowen <e.bowen.wang@icloud.com>
2018-06-04 15:44:13 +0800

    adjust the event handler order

--- aixbot.js
+++ aixbot.js
@@ -117,7 +117,6 @@ class AixBot {
         if (await this.doHandle(ctx, this.eventListeners.recordFinish, ctx.request.isRecordFinish)) return;
         if (await this.doHandle(ctx, this.eventListeners.recordFail, ctx.request.isRecordFail)) return;
         if (await this.doHandle(ctx, this.eventListeners.playFinishing, ctx.request.isPlayFinishing)) return;
-        if (await this.doHandle(ctx, this.eventListeners.inSkill, ctx.request.isInSkill)) return;
         if (await this.doHandle(ctx, this.intentListeners[ctx.request.intent], () => {
             return this.intentListeners.hasOwnProperty(ctx.request.intent);
         })) return;
@@ -125,6 +124,7 @@ class AixBot {
             return this.textListeners.hasOwnProperty(ctx.request.query);
         })) return;
         if (await this.doHandle(ctx, this.getRegExpHandler(ctx.request.query))) return;
+        if (await this.doHandle(ctx, this.eventListeners.inSkill, ctx.request.isInSkill)) return;
     }
 
     async doHandle(ctx, handler, trigger) {
